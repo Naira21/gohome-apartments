b658b7cc0360171c2cf0361db8cd8ff2ead7aab8
Naira Kazeian <nkazeian@gmail.com>
2023-01-10 17:14:22 +0200

    counter and button

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :id="$style.app">
+    <h2>{{ amountOfClicks() }}</h2>
+    <ButtonVue @click="increment">Click</ButtonVue>
+  </div>
+</template>
+
+<script>
+import ButtonVue from "./components/Button.vue";
+import { ref } from "vue";
+export default {
+  name: "App",
+  components: {
+    ButtonVue,
+  },
+  setup() {
+    const allClicks = ref(0);
+    function increment() {
+      allClicks.value += 1;
+    }
+    function amountOfClicks() {
+      return `Amount of clicks ${allClicks.value}`;
+    }
+    return { allClicks, amountOfClicks, increment };
+  },
+};
+</script>
+
+<style module>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StarRating.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="star-rating">
+    <span class="star"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StarRating",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
