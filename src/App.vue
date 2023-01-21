5d119ac886f3f9fe76215c22ceadc4648aa83452
Naira Kazeian <nkazeian@gmail.com>
2023-01-21 22:56:46 +0200

    created apartments list

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,78 @@
 <template>
   <div :id="$style.app">
-    <h2>{{ amountOfClicks() }}</h2>
-    <StarRating :rating="4.5" />
+    <ApartmentsList :items="apartment" />
   </div>
 </template>
 
 <script>
-// import ButtonVue from "./components/Button.vue";
-import StarRating from "./components/StarRating.vue";
 import { ref } from "vue";
+import ApartmentsList from "./components/apartment/ApartmentsList.vue";
 
 export default {
   name: "App",
   components: {
-    // ButtonVue,
-    StarRating,
+    ApartmentsList,
   },
   setup() {
-    const allClicks = ref(0);
-    function increment() {
-      allClicks.value += 1;
-    }
-    function amountOfClicks() {
-      return `Amount of clicks ${allClicks.value}`;
-    }
-    return { allClicks, amountOfClicks, increment };
+    const apartment = ref([
+      {
+        id: "5f05c9dad2c9bc0f773444bc",
+        title:
+          "Aut qui adipisci distinctio maiores molestiae sit est inventore vero.",
+        descr:
+          "Non perferendis rerum a in nisi exercitationem dolorum perferendis. Eligendi sit error sed a. Facere maiores sit adipisci sequi eveniet. Qui est voluptatum maiores eos qui vitae.",
+        price: 2032,
+        rating: 5,
+        location: {
+          city: "Kherson",
+        },
+      },
+      {
+        id: "5f05c9dad2c9bc0f773444bc",
+        title:
+          "Aut qui adipisci distinctio maiores molestiae sit est inventore vero.",
+        descr:
+          "Non perferendis rerum a in nisi exercitationem dolorum perferendis. Eligendi sit error sed a. Facere maiores sit adipisci sequi eveniet. Qui est voluptatum maiores eos qui vitae.",
+        price: 2032,
+        rating: 1,
+        location: {
+          city: "Kherson",
+        },
+      },
+      {
+        id: "5f05c9dad2c9bc0f773444bc",
+        title:
+          "Aut qui adipisci distinctio maiores molestiae sit est inventore vero.",
+        descr:
+          "Non perferendis rerum a in nisi exercitationem dolorum perferendis. Eligendi sit error sed a. Facere maiores sit adipisci sequi eveniet. Qui est voluptatum maiores eos qui vitae.",
+        price: 2032,
+        rating: 4.7,
+        location: {
+          city: "Kherson",
+        },
+      },
+    ]);
+    const owner = ref({
+      name: "Ellen",
+      phone: "115-355-5652",
+      email: "Tracey.Morar86@hotmail.com",
+    });
+    // const allClicks = ref(0);
+    // function increment() {
+    //   allClicks.value += 1;
+    // }
+    // function amountOfClicks() {
+    //   return `Amount of clicks ${allClicks.value}`;
+    // }
+    // return { allClicks, amountOfClicks, increment };
+    return { owner, apartment };
   },
 };
 </script>
 
 <style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/apartment/ApartmentsList.vue
@@ -0,0 +1,49 @@
+<template>
+  <Container>
+    <div class="apartments-list">
+      <ApartmentsItem
+        v-for="{ id, descr, price, rating } in items"
+        :key="id"
+        :descr="descr"
+        :price="price"
+        :rating="rating"
+        class="apartments-list__item"
+      />
+      <!-- :imgUrl="" -->
+    </div>
+  </Container>
+</template>
+
+<script>
+import ApartmentsItem from "./ApartmentsItem.vue";
+import Container from "../shared/Container.vue";
+
+export default {
+  name: "ApartmentsList",
+  components: {
+    ApartmentsItem,
+    Container,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartments-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+  &__item {
+    margin-bottom: 30px;
+  }
+}
+</style>
